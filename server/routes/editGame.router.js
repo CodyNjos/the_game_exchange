cd4a2d6021e4f3f4a120c0c14201ffbfd220bef4
Cody Njos <cody.njos@live.com>
2021-02-22 19:25:13 -0600

    created seperate router for edits. Moved put requtest for tradable status to that route

---
+++ server/routes/editGame.router.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const pool = require('../modules/pool');
+const router = express.Router();
+
+//EDIT GAME TRADEABLE ROUTER
+router.put('/tradeable/:id', (req, res) => {
+    // req.body should contain a category_id to add to this favorite image
+    const id = req.params.id;
+    const boolean = req.body.boolean;
+    console.log(`Updating the game with id:${id}`);
+    console.log(id, boolean)
+    const queryText = `UPDATE "games" SET "tradeable" = $1
+                      WHERE "id" = $2;`;
+  
+    pool.query(queryText, [boolean, id]).then(() => {
+      console.log(`Updated successfully`);
+      res.sendStatus(200);
+    }).catch(err => {
+      console.log('Error in update', err);
+      res.sendStatus(500);
+    });
+  });
+
+module.exports = router
--- server/routes/game.router.js
+++ server/routes/game.router.js
@@ -14,7 +14,6 @@ router.get('/', (req, res) => {
         res.sendStatus(500)
     })
   });
-  module.exports = router
 
 
 //ADD GAMES ROUTER
@@ -45,22 +44,5 @@ router.get('/', (req, res) => {
     });
   });
 
-  //EDIT GAMES ROUTER
-  router.put('/tradeable/:id', (req, res) => {
-    // req.body should contain a category_id to add to this favorite image
-    const id = req.params.id;
-    const boolean = req.body.boolean;
-    console.log(`Updating the game with id:${id}`);
-    console.log(id, boolean)
-    const queryText = `UPDATE "games" SET "tradeable" = $1
-                      WHERE "id" = $2;`;
   
-    pool.query(queryText, [boolean, id]).then(() => {
-      console.log(`Updated successfully`);
-      res.sendStatus(200);
-    }).catch(err => {
-      console.log('Error in update', err);
-      res.sendStatus(500);
-    });
-  });
-  
+  module.exports = router
