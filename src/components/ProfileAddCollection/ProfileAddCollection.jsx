c74db188488a94b98fa47d3f2a609698532eacaf
Cody Njos <cody.njos@live.com>
2021-02-23 10:21:50 -0600

    input prints objets to console using the values of local state, and the value of user redux store

--- src/components/ProfileAddCollection/ProfileAddCollection.jsx
+++ src/components/ProfileAddCollection/ProfileAddCollection.jsx
@@ -1,24 +1,34 @@
-import {useState} from 'react'
-
+import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 function ProfileAddCollection() {
-    const [ form, setForm ] = useState(true)
+    const store = useSelector(store => store)
+    const dispatch = useDispatch();
+    const [form, setForm] = useState(true)
     const toggleForm = () => {
         setForm(current => !current)
         console.log(form)
     }
-    return ( form?
+    const [newGame, setNewGame] = useState({
+        user_id: store.user.id,
+        game_name: "",
+        img_url: ""
+    })
+    const addGame = () => {
+        console.log(newGame)
+        // dispatch({type:'ADD_GAME', payload:newGame})
+    }
+    return (form ?
         <>
 
             <button onClick={toggleForm}>Toggle</button>
-            </>
-            :
-            <>
-            <input placeholder='Title' />
-            <input placeholder='Image Url' />
-            <select>
-                <option>No</option>
-                <option>Yes</option>
-            </select>
+        </>
+        :
+        <>
+            <form onSubmit={addGame}>
+                <input value={newGame.game_name} onChange={(e) => setNewGame ({ ...newGame, game_name: e.target.value})} placeholder='Title' />
+                <input value={newGame.img_url} onChange={(e) => setNewGame ({ ...newGame, img_url: e.target.value})} placeholder='Image Url' />
+                <button type='submit'>Submit</button>
+            </form>
             <button onClick={toggleForm}>Go Back</button>
         </>
     )
