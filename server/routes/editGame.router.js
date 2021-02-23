039ea24cd3068c94cd3a577e6146222f1d264fdf
Cody Njos <cody.njos@live.com>
2021-02-22 20:44:31 -0600

    added put request to edit img_url. Works in postman

--- server/routes/editGame.router.js
+++ server/routes/editGame.router.js
@@ -7,12 +7,29 @@ router.put('/tradeable/:id', (req, res) => {
     // req.body should contain a category_id to add to this favorite image
     const id = req.params.id;
     const boolean = req.body.boolean;
-    console.log(`Updating the game with id:${id}`);
+    console.log(`Updating the game with id:${id} to set its tradeable value to:${boolean}`);
     console.log(id, boolean)
-    const queryText = `UPDATE "games" SET "tradeable" = $1
+    const queryText = `UPDATE "games" SET "tradeable" = $1 SET "img_url
                       WHERE "id" = $2;`;
   
     pool.query(queryText, [boolean, id]).then(() => {
+      console.log(`Updated successfully`);
+      res.sendStatus(200);
+    }).catch(err => {
+      console.log('Error in update', err);
+      res.sendStatus(500);
+    });
+  });
+  router.put('/image/:id', (req, res) => {
+    // req.body should contain a category_id to add to this favorite image
+    const id = req.params.id;
+    const image = req.body.img_url;
+    console.log(`Updating the game with id:${id}`);
+    console.log(id, image)
+    const queryText = `UPDATE "games" SET "img_url" = $1
+                      WHERE "id" = $2;`;
+  
+    pool.query(queryText, [image, id]).then(() => {
       console.log(`Updated successfully`);
       res.sendStatus(200);
     }).catch(err => {
