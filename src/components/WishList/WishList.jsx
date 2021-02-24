564eec22a630114676e12e3ab116e28cd8df727b
Cody Njos <cody.njos@live.com>
2021-02-24 13:12:35 -0600

    wishlisht page filteres all games and only displays games on wishlists

--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -1,7 +1,23 @@
+import {useSelector, useDispatch} from "react-redux"
+import {useEffect, useState} from 'react'
+
 function WishList() {
+    const dispatch = useDispatch();
+    const store = useSelector(store => store);
+    useEffect(() => {
+        dispatch({ type: 'GET_GAMES' });
+    }, []);
+    const wishlist = store.games.filter(games => games.wish_list === true)
     return(
         <>
         <p>Wish List Page</p>
+        {wishlist.map(games => {
+            return(
+            <div key = {games.id}>
+                <p>{games.game_name}</p> <br/>
+                <img src={games.img_url}/>
+            </div>
+        )})}
         </>
     )
 }
