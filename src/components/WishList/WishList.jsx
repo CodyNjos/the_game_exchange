f52f0ea076ea8f10d3c1856cf6bca9db006ac810
Cody Njos <cody.njos@live.com>
2021-04-09 22:32:43 -0500

    added pageination to wishlist page

--- src/components/WishList/WishList.jsx
+++ src/components/WishList/WishList.jsx
@@ -3,16 +3,28 @@ import { useEffect, useState } from 'react'
 import { useHistory } from "react-router-dom";
 import { TextField, Button, Paper, Card, CardContent } from '@material-ui/core'
 import { useStyles } from '../GameCardStyle/GameCardStyle'
-
+import { Pagination } from '@material-ui/lab'
 function WishList() {
     const dispatch = useDispatch();
     const history = useHistory()
     const store = useSelector(store => store);
     const [search, setSearch] = useState('')
+    const wishlist = store.games.filter(games => games.wish_list === true)
     useEffect(() => {
         dispatch({ type: 'GET_GAMES', payload: search });
     }, [search]);
-    const wishlist = store.games.filter(games => games.wish_list === true)
+    useEffect(() => {
+        setNoOfPages(Math.ceil(wishlist.length / itemsPerPage));
+    }, [store.games])
+
+    
+    const [page, setPage] = useState(1);
+    const itemsPerPage = 6;
+    const [noOfPages, setNoOfPages] = useState(Math.ceil(wishlist.length / itemsPerPage))
+    const handleChange = (event, value) => {
+        setPage(value);
+    }
+   
     const viewProfile = (user) => {
         console.log(user)
         if (user === store.user.id) {
@@ -30,7 +42,7 @@ function WishList() {
             {wishlist.length > 0 ?
                 <>
                     <div className='cardWrap'>
-                        {wishlist.map(games => {
+                        {wishlist.slice((page - 1) * itemsPerPage, page * itemsPerPage).map(games => {
                             return (
                                 <div key={games.id} className="gameCard" >
                                     <Paper className={classes.paper} elevation={20}>
@@ -46,6 +58,18 @@ function WishList() {
                                 </div>
                             )
                         })}
+                    </div>
+                    <div className="pageWrap">
+                        <Pagination
+                            style = {{margin:"auto"}}
+                            className="pagination"
+                            count={noOfPages}
+                            shape="rounded"
+                            variant="outlined"
+                            onChange={handleChange}
+                            defaultPage={1}
+                            showFirstButton
+                            showLastButton />
                     </div>
                 </>
                 :
