adf42aae71d261cd54cc0a63bc510cbbe4ce36ef
Cody Njos <cody.njos@live.com>
2021-04-13 13:52:16 -0500

    added nav drawer for phones and tablets

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,12 +1,16 @@
 import React from 'react';
+import { useState } from 'react'
 import { Link } from 'react-router-dom';
 import LogOutButton from '../LogOutButton/LogOutButton';
+import { useMediaQuery, Drawer, Button, makeStyles } from '@material-ui/core'
+import MenuIcon from "@material-ui/icons/Menu";
 import './Nav.css';
 import { useSelector } from 'react-redux';
 
 function Nav() {
   const user = useSelector((store) => store.user);
-
+  const shrink = useMediaQuery("(min-width: 1200px)")
+  const [drawer, setDrawer] = useState(true);
   let loginLinkData = {
     path: '/login',
     text: 'Login / Register',
@@ -16,37 +20,83 @@ function Nav() {
     loginLinkData.path = '/user';
     loginLinkData.text = 'Home';
   }
-
+  const useStyles = makeStyles((theme) => ({
+    drawerLink: {
+      color: "white",
+      backgroundColor: "#3f51b5",
+      borderRadius: 0,
+      padding: "20px  5px 20px 5px",
+    },
+    drawerPaper: {
+      backgroundColor: "#FF0000"
+    }
+  }));
+  const classes = useStyles()
   return (
     <div className="nav">
       <Link to="/home">
         <h2 className="nav-title">The Game Exchange</h2>
       </Link>
-      <div>
-        <Link className="navLink" to='/homepage'>
-          {/* to={loginLinkData.path}> {loginLinkData.text} */}
+      {shrink ?
+        <div>
+          <Link className="navLink" to='/homepage'>
+            {/* to={loginLinkData.path}> {loginLinkData.text} */}
         Home
         </Link>
-        <Link className="navLink" to="/profile">
-          Profile
+          <Link className="navLink" to="/profile">
+            Profile
         </Link>
 
-        {/* {user.id && (
+          {/* {user.id && (
           <>
             <Link className="navLink" to="/info">
               Info Page
             </Link>
           </>
         )} */}
-        <Link className="navLink" to="/wishlist">
-          In Demand
+          <Link className="navLink" to="/wishlist">
+            In Demand
         </Link>
-        <Link className="navLink" to="/about">
-          About
+          <Link className="navLink" to="/about">
+            About
         </Link>
-        <LogOutButton className="navLink" />
+          <LogOutButton className="navLink" />
 
-      </div>
+        </div>
+        :
+        <>
+          <Button style={{ color: "white" }} onClick={() => !drawer ? setDrawer(true) : setDrawer(false)} >
+            Menu
+            <MenuIcon />
+          </Button>
+          <Drawer
+            anchor="top"
+            open={drawer}
+            onClick={() => setDrawer(false)}
+          >
+             <Button
+                  className={classes.drawerLink}
+                  component={Link}
+                  to="/homepage"
+              >
+                Home
+              </Button>
+              <Button
+                  className={classes.drawerLink}
+                  component={Link}
+                  to="/profile"
+              >
+                Profile
+              </Button>
+              <Button
+                  className={classes.drawerLink}
+                  component={Link}
+                  to="/about"
+              >
+                About
+              </Button>
+              <LogOutButton className="navLink" />
+          </Drawer></>}
     </div>
   );
 }
