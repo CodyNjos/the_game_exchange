0a8b14e3ebb907c65c441339b2a1e73020991332
Cody Njos <cody.njos@live.com>
2021-08-10 15:22:52 -0500

    modified responsive landing page

--- src/components/LandingPage/LandingPage.jsx
+++ src/components/LandingPage/LandingPage.jsx
@@ -7,7 +7,7 @@ import { useMediaQuery } from '@material-ui/core'
 import LoginForm from '../LoginForm/LoginForm'
 
 function LandingPage() {
-
+  
   const shrink = useMediaQuery("(min-width: 1200px)")
   const history = useHistory();
 
@@ -17,10 +17,12 @@ function LandingPage() {
 
   return (
     <div className="container">
-      {shrink ?
+
+
       <div className="grid">
-        <div className="grid-col grid-col_8">
+        <div className= {shrink?"grid-col grid-col_8" : "mobileWelcomeText"}>
           <h2>Welcome To The Game Exchange!</h2>
+          {/* ¡!¡! THIS STILL NEEDS A TON OF WORK ¡!¡!*/}
           <p>
             Here at The Game Exchange, we know that regardless of how much you treasure your game collection,
             there's always some outliers that you just dont play anymore. Maybe you've even set aside a dark corner
@@ -42,7 +44,7 @@ function LandingPage() {
            
           </p>
         </div>
-        <div className="grid-col grid-col_4">
+        <div className={ shrink ? "grid-col grid-col_4" : "mobileLoginForm"}>
           {/* <RegisterForm /> */}
           <LoginForm />
           <center>
@@ -53,46 +55,6 @@ function LandingPage() {
           </center>
         </div>
       </div>
-      :
-      <div>
-  
-        <div >
-          <h2>Welcome To The Game Exchange!</h2>
-          <div className="mobileLoginForm">
-          {/* <RegisterForm /> */}
-          <LoginForm />
-          <center>
-            <h4>Don't Have An Account?</h4>
-            <button className="btn btn_sizeSm" onClick={onLogin}>
-              Create Account
-            </button>
-          </center>
-        </div>
-        <div className="mobileWelcomeText">
-          <p>
-            Here at The Game Exchange, we know that regardless of how much you treasure your game collection,
-            there's always some outliers that you just dont play anymore. Maybe you've even set aside a dark corner
-            of closet to store such games, and have forget what all is back there.
-          </p>
-
-          <p>
-            We would like to provide a platform to not only catalog all your games, but also get excess games out of your closet,
-            and exchange them for games you'll get more playtime out of.
-          </p>
-
-          <p>
-            If you do not have an account, please click the "Create Account" button to get started. Please keep in mind that the email you register with will be
-            the email given to other users to contanct you. Once you've created an account, you can navigate to the homepage. Here you will find a list all the games users
-            are willing to trade away. On the In Demand page, you will find a list of all the games users would like to recive in a trade.
-            If you would like to add games to you collection, and your wishlist navigate to "My Profile". here you will find buttons to add games to you collection and wishlist. If a game
-            is marked tradeable, users will see your game on the homepage.
-            To mark a game as "tradeable", simply click the edit button in your collection section. This will allow you to mark the game as tradeable, as well as edit the games description.
-           
-          </p>
-        </div>
-      </div>
-      </div>
-}
     </div>
   );
 }
