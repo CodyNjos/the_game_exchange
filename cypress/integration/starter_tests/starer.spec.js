f41191ad4a3aeadbbeb9493e59067567d2f156ae
Cody Njos <cody.njos@live.com>
2021-08-10 17:40:44 -0500

    moved login test to seperate function for re-use

--- cypress/integration/starter_tests/starer.spec.js
+++ cypress/integration/starter_tests/starer.spec.js
@@ -1,9 +1,22 @@
+const loginFunction = () => {
+    const login = "Cypress"
+        const password = "123" //should be accessed from Database
+        cy.get('#username').should('have.value', '')
+        cy.get('#password').should('have.value', '')
+        cy.get('#username').type(`${login}`) // Not best practice to target like this, needs updating https://docs.cypress.io/guides/references/best-practices#Selecting-Elements
+        cy.get('#password').type(`${password}`)
+        cy.get('#username').should(`have.value`, `${login}`)
+        cy.get('#password').should(`have.value`, `${password}`)
+        cy.get('#loginButton').click()
+}
+
+
 describe('Passing Test', () => {
     it('Will Pass', () => {
       expect(true).to.equal(true)
     })
   })
-  
+
 // describe('Failing test', () => {
 //     it('Will Fail', () => {
 //         expect(true).to.equal(false)
@@ -12,18 +25,28 @@ describe('Passing Test', () => {
   
 describe('Login Test', () => {
     beforeEach(() => {
-       
+        cy.viewport('macbook-13')
         cy.visit('http://localhost:3000/#/home')
-        cy.viewport(1500, 700)
+        
    
     })
-    it('type in login', () => { 
-        const login = "Cypress"
+    it('Login Success', () => { // 
+        loginFunction();
+    })
+    it('Login Failure', () => { 
+        const login = "NotAUser"
         const password = "123" //should be accessed from Database
+        cy.get('#username').should('have.value', '')
+        cy.get('#password').should('have.value', '')
         cy.get('#username').type(`${login}`) // Not best practice to target like this, needs updating https://docs.cypress.io/guides/references/best-practices#Selecting-Elements
         cy.get('#password').type(`${password}`)
+        cy.get('#username').should(`have.value`, `${login}`)
+        cy.get('#password').should(`have.value`, `${password}`)
+        cy.get('.alert').should('not.exist')
         cy.get('#loginButton').click()
+        cy.get('.alert').should('exist')
     })
+
 })
 
 describe('Reactive Nav Test', () => {
@@ -37,5 +60,11 @@ describe('Reactive Nav Test', () => {
     })
     it('Test Mobile (Pixel 2)', () => { 
         cy.viewport(411, 731)
+    })
+})
+
+describe('Add And Remove Game', () => {
+    it('Test outside function', () => {
+        loginFunction()
     })
 })
