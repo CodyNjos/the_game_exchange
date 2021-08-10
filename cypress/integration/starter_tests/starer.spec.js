ed2e28b373a7a38ac69e2a2e26f5e9e36223e9a9
Cody Njos <cody.njos@live.com>
2021-08-10 16:55:16 -0500

    added additional tests to login

--- cypress/integration/starter_tests/starer.spec.js
+++ cypress/integration/starter_tests/starer.spec.js
@@ -4,11 +4,11 @@ describe('Passing Test', () => {
     })
   })
   
-// describe('Failing test', () => {
-//     it('Will Fail', () => {
-//         expect(true).to.equal(false)
-//     })
-// })
+describe('Failing test', () => {
+    it('Will Fail', () => {
+        expect(true).to.equal(false)
+    })
+})
   
 describe('Login Test', () => {
     beforeEach(() => {
@@ -18,31 +18,35 @@ describe('Login Test', () => {
    
     })
     it('type in login', () => { 
+        cy.get('#username').should('have.value', '')
+        cy.get('#password').should('have.value', '')
         const login = "Cypress"
         const password = "123" //should be accessed from Database
         cy.get('#username').type(`${login}`) // Not best practice to target like this, needs updating https://docs.cypress.io/guides/references/best-practices#Selecting-Elements
         cy.get('#password').type(`${password}`)
+        cy.get('#username').should(`have.value`, `${login}`)
+        cy.get('#password').should(`have.value`, `${password}`)
         cy.get('#loginButton').click()
     })
 })
 
-describe('Reactive Nav Test', () => {
+describe('Responsive Nav Test', () => {
     beforeEach(() => {
        
         cy.visit('http://localhost:3000/#/home')
    
     })
-    it('Test Responsive (Iphone X )', () => { 
+    it('Test Responsive Nav (Iphone X )', () => { 
         cy.viewport(375, 812)
         cy.get('.nav button').should('have.length', 1)
         cy.get('.nav a').should('have.length', 1)
     })
-    it('Test Responsive (Pixel 2)', () => { 
+    it('Test Responsive Nav(Pixel 2)', () => { 
         cy.viewport(411, 731)
         cy.get('.nav button').should('have.length', 1)
         cy.get('.nav a').should('have.length', 1)
     })
-    it('Test Responsive (13in MacBook Pro)', () => { 
+    it('Test Responsive Nav (13in MacBook Pro)', () => { 
         cy.viewport(2560, 1600)
         cy.get('.nav button').should('have.length', 1)
         cy.get('.nav a').should('have.length', 5)
